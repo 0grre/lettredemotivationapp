2f8cd114e66162d8e3e76963b64b36cd667e3022
ogrre <jbloup@hotmail.fr>
2023-11-07 11:24:54 +0100

    fix(fields): remove required twice

--- resources/views/first-letter/steps/form-job.blade.php
+++ resources/views/first-letter/steps/form-job.blade.php
@@ -58,7 +58,7 @@
                 <label for="experience" class="block mb-3 text-sm font-medium text-gray-900 dark:text-white">
                     Ton expérience
                 </label>
-                <select id="experience" required name="experience" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" required>
+                <select id="experience" name="experience" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" required>
                     <option disabled selected>On a besoin de connaître vos années d'expériences</option>
                     <option value="0">C'est ma première fois</option>
                     <option value="1">1 an ou moins</option>
